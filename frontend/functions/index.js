addcc6d0865eb6b765c9176c3b1849ef97653efb
Taxman3 <robinsonbarasa22@gmail.com>
2025-03-05 17:31:16 +0300

    Fix ESLint errors in Firebase Functions

--- frontend/functions/index.js
+++ frontend/functions/index.js
@@ -1,19 +1,17 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// Your function code here...
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Uncomment the function below only if you intend to use it
+/*
+const { onRequest } = require("firebase-functions/v2/https");
+const { logger } = require("firebase-functions");
+
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
+*/
